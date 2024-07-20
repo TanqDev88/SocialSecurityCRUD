e318cb8f7a0da4937b11665bd5d445803acb377f
tanqueta88 <oliverprim783@gmail.com>
2024-07-19 23:47:35 -0300

    App.js file is added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CrudComponent from './components/CrudComponent';
+
+function App() {
+  return (
+    <div className="bg-gray-800 text-white min-h-screen flex justify-center ">
+      <CrudComponent />
+    </div>
+  );
+}
+
+export default App;
